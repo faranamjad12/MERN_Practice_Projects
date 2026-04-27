194f1d16d332f9be5edc171f1fc005ed8c11b73b
faranamjad12 <faran4500485@gmail.com>
2026-04-27 19:59:24 +0500

    Update NoteItem.jsx

--- frontend/components/notes/NoteItem.jsx
+++ frontend/components/notes/NoteItem.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const NoteItem = ({item}) => {
   return (
-    <>
+    <div style={{backgroundColor: item.color}}>
      <h2 className="font-semibold">{item.title}</h2>
       <p className="text-sm text-gray-500 truncate">{item.content}</p> 
-      </>
+      </div>
   );
 };
 
